7e731608a622af4e6d194bc0ed610a8b43f3a49e
Robbie <Robbie@email.com>
2024-12-29 16:16:22 +0900

    :tada: init : 초기 파일 설정

---
+++ src/test/java/com/springboot/mealkart/MealKartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.mealkart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealKartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
